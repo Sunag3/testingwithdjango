3392057807cef09d4b529e56f960ef4c64c759ec
Sunag <sunag.shetty4@gmail.com>
2021-09-01 00:33:14 +0530

    added Actions

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 # Changes in Gitgit a
 # Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=225)
+    author = models.CharField(max_length=225)
+    slug = models.SlugField(max_length=225)
+
+    def __str__(self):
+        return self.title
--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
-
+from .models import Post
 # Create your tests here.
+
+
+class ModelTesting(TestCase):
+    def setUp(self):
+        self.blog = Post.objects.create(
+            title="Testing Title", author="Sunag", slug="Sunag")
+
+    def test_post_model(self):
+        d = self.blog
+        self.assertTrue(isinstance(d, Post))
+        self.assertEqual(str(d), 'Testing Title')
